89130d6f20241c07f91b34fbaa66d4f6f8575ad2
Jehyeon Lee <89868567+jaydooroo@users.noreply.github.com>
2025-07-22 16:41:14 -0500

    backend and frontend works well

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router';
 import { TradingPageComponent } from './pages/trading-page/trading-page.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: '/home', component: TradingPageComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: TradingPageComponent },
 ];
